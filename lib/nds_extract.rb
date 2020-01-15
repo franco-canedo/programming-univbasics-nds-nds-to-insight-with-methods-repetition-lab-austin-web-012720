901a76d8388f6d776d9f496957b174d499d644ef
Franco Canedo <francanedo1@hotmail.com>
2020-01-15 21:02:32 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -49,7 +49,7 @@ def total_gross(source)
   hash = directors_totals(source)
   index = 0 
   while index < hash.length do 
-   total += hash[index]
+   total += hash[index][list_of_directors[index]]
    index += 1
   end
   
